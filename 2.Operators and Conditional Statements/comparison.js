dd5f3db9622db56386dfb5431e671247624bdd24
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-20 22:20:11 +0530

    Comparison operator.

---
+++ 2.Operators and Conditional Statements/comparison.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = "5";
+
+console.log("a == b", a == b) //true.
+console.log("a != b", a == b) //true
+console.log("a === b", a === b) //false - checks type too.
+console.log("a != b", a == b) //true - also checks type.
+
+let c = 4;
+let d = 5;
+
+console.log("c > d =", c > d);
+console.log("c < d =", c < d);
+console.log("c >= d =", c >= d);
+console.log("c <= d =",c <= d);
