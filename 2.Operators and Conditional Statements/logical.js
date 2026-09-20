f2efaf260e2bcfe01ed9b79726c5e54ea39a2351
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-20 22:35:05 +0530

    this are the logical statements

---
+++ 2.Operators and Conditional Statements/logical.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b = 12;
+
+console.log("a && b > 15",a && b > 15);
+console.log("cond || cond", a > b || a < b);
+console.log("!(a || b > 15)",!(a || b > 11)); // real ans is true but we used not so it will give false.
