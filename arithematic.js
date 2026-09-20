ca579c829dffc397b4323fbe06b4862d540b2a90
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-20 21:56:26 +0530

    day 2

---
+++ arithematic.js
@@ -0,0 +1,12 @@
+let a =20;
+let b = 5;
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b); //Exponential operator a^b.
+console.log("a ++ =", a++); //post 
+console.log("b-- =",b--);  //post
+
+
