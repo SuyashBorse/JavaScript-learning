191b08da088f62ca1296d5e0a2eb947adae411df
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-21 21:15:45 +0530

    practice Q1

---
+++ 3.Loops and String/EvenNum.js
@@ -0,0 +1,8 @@
+// Q1 - to print even no from 1 t0 100.
+
+console.log("The even numbers from 0 to 100 are:");
+for(let i =0 ; i <= 100 ; i++){
+    if( i % 2 == 0){
+        console.log(i);
+    }
+}
