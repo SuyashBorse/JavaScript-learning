cfc7034a317c5f6d4b829888125b5af9bd8850db
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-21 22:01:50 +0530

    escape characters in js

--- 3.Loops and String/templateLiterals.js
+++ 3.Loops and String/templateLiterals.js
@@ -8,3 +8,8 @@ let favSuperhero  = {
     slogan : "MAN ARE BRAVE.",
 };
 console.log(`So my fav char is a ${favSuperhero.name} by profession he is ${favSuperhero.Proffesion} he had ${favSuperhero.power} of powers his slogan is ${favSuperhero.slogan}`);
+
+// Escape characters -
+
+console.log("this is the \n escape character ");  // adds a new line at the place of \n.
+console.log("this is the \t escape character ");  // adds a Tab space at the place of \t.
