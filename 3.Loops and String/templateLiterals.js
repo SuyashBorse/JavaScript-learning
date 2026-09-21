63aa7c15d57787640dfc005fab50ef5ff930f413
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-21 21:58:39 +0530

    template literal example

---
+++ 3.Loops and String/templateLiterals.js
@@ -0,0 +1,10 @@
+// It is a way to have embedded expressions in string.
+
+let favSuperhero  = {
+    name : "Batman",
+    Proffesion : "superhero , Billionaire",
+    power : "no need",
+    age : 50,
+    slogan : "MAN ARE BRAVE.",
+};
+console.log(`So my fav char is a ${favSuperhero.name} by profession he is ${favSuperhero.Proffesion} he had ${favSuperhero.power} of powers his slogan is ${favSuperhero.slogan}`);
