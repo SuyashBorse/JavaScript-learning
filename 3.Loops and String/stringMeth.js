bb1f1f349b7fadf011d1fbd30f4821555622ffbb
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-21 22:32:34 +0530

    String methods in js

---
+++ 3.Loops and String/stringMeth.js
@@ -0,0 +1,18 @@
+// String methods 
+
+let str1 = "   Hello";
+let str2 = "world!";
+
+console.log(str1.toUpperCase());
+console.log(str2.toLowerCase());
+
+console.log(str1.trim());
+
+console.log(str2.slice(1,3));
+
+console.log(str1.concat(str2));
+
+console.log(str2.replace("w" , "b"));
+console.log(str1.replaceAll("l", "p"));
+
+console.log(str2.charAt(2));
