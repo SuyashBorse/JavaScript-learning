788d465f0237c4d126d732c4077f191ec303e2f1
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-21 21:49:17 +0530

    Strings in js

---
+++ 3.Loops and String/string.js
@@ -0,0 +1,11 @@
+let str1 = "Hello";
+let str2 = "world";
+
+// 1. string.length.
+console.log(str1.length);
+console.log(str2.length);
+
+// 2. string indices - index - always starts fron 0.
+console.log(str1[0]);
+console.log(str1[3]);
+console.log(str2[4]);
