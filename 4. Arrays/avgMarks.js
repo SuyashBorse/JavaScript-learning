e438594cd287af1a999dca08d82ca6cfb73733ec
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-22 15:31:51 +0530

    Prac Q1

---
+++ 4. Arrays/avgMarks.js
@@ -0,0 +1,9 @@
+let marks = [85,97,44,37,76,60];
+
+let sum = 0;
+
+for(let avg of marks){
+   sum += avg ; 
+} 
+let average = sum / marks.length;
+console.log("so the average marks are :" , average);
