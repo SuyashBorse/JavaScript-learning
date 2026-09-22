cee4ca66a9302a5ba9f729c1e2d346d6dd4d2207
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-22 15:20:25 +0530

    Arrays in js

---
+++ 4. Arrays/Array1.js
@@ -0,0 +1,13 @@
+let superHeroes = ["Ironman" , "Thor" , "Caption America" , "Batman" , "Superman" , "Shaktiman"];
+
+//print using for loop 
+
+for(let i = 0; i < superHeroes.length; i++){
+    console.log(superHeroes[i]);
+}
+
+// Print using for off loop 
+
+for(let x of superHeroes){
+    console.log(x);
+}
