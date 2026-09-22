eb44cacc750a090a51163563f1158cc57838f0b3
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-22 23:15:01 +0530

    Pop method

--- 4. Arrays/arrayMeth.js
+++ 4. Arrays/arrayMeth.js
@@ -1,5 +1,11 @@
 let codingLang = ["JavaScript" , "Java" , "Python" , "PHP"];
 console.log(codingLang);
 
+// Push method 
 codingLang.push("Rust" , "CPP" , "Swift" , "Go");
+console.log(codingLang);
+
+// Pop method
+codingLang.pop();
+codingLang.pop();
 console.log(codingLang);
