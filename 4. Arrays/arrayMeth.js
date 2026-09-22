6022d4b493a73f1eee360a9b005e61acbe399d39
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-22 23:20:04 +0530

    Concat method

--- 4. Arrays/arrayMeth.js
+++ 4. Arrays/arrayMeth.js
@@ -14,3 +14,9 @@ console.log(codingLang);
 let marks = [66,78,89,90,84];
 console.log(marks);
 console.log(marks.toString());  // it always returns a new array instead of changing the first array.
+
+// concat method 
+let marvelSupHeroes = ["IronMan" , "Thor", "Captain America"];
+let dcSupHeroes = ["Batman" , "Superman" , "Flash"];
+let supHeroes = marvelSupHeroes.concat(dcSupHeroes); //it also always returns a new array.
+console.log(supHeroes);
