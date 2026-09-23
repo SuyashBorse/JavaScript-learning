c88f560d832f8734b0ab1bf598dc610b0e81329a
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-23 23:02:08 +0530

    Arrow function in JS

---
+++ 5. Functions and Methods/arrowFunction.js
@@ -0,0 +1,14 @@
+let result = (a , b) => {
+    return a + b;
+}
+ 
+
+let subResult = (x , y) => {
+    console.log(x - y);
+}
+
+console.log(result);
+console.log(subResult);
+
+console.log(result(10 , 12));
+subResult(12 , 10);
