300e29519b0c6c361585ae7673d70f537be7a33d
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-25 22:44:21 +0530

    innerHtml in DOM

--- 6. DOM-1/propertiesDOM.js
+++ 6. DOM-1/propertiesDOM.js
@@ -2,4 +2,8 @@
 console.log(firstElement.tagName);   //Gives tag name 
 
 // heading.innerText;
-console.log(firstElement.innerText);
+console.log(firstElement.innerText); 
+
+let div1 = document.querySelectorAll("div");
+console.log(div1);
+console.log(div1.innerText);
