14ac002442cb9bca6dfcdb6779abf7688e735060
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-25 21:01:42 +0530

    accessing Html using DOM by class

--- 6. DOM-1/accessInDom.js
+++ 6. DOM-1/accessInDom.js
@@ -1,2 +1,6 @@
-let heading = document.getElementById("heading");
-console.dir(heading);
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+let heading1 = document.getElementsByClassName("colour1");
+console.dir(heading1);
+console.log(heading1);
