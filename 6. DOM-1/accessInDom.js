cc5eacc34d498cf467388b632bb361b2598d7b5a
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-25 21:09:26 +0530

    accessing html element in DOM by tag

--- 6. DOM-1/accessInDom.js
+++ 6. DOM-1/accessInDom.js
@@ -1,6 +1,10 @@
-// let heading = document.getElementById("heading");
-// console.dir(heading);
+ let heading = document.getElementById("heading");
+ console.dir(heading);
 
 let heading1 = document.getElementsByClassName("colour1");
 console.dir(heading1);
 console.log(heading1);
+
+let para = document.getElementsByTagName("p");
+console.dir(para);
+console.log(para);
