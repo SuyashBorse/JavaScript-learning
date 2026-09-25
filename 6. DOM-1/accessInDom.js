64b077228c13c577b91d274781f5f7ab3d411201
Suyash Borse <suyash.a.borse@gmail.com>
2026-09-25 21:33:10 +0530

    Query selector in dom

--- 6. DOM-1/accessInDom.js
+++ 6. DOM-1/accessInDom.js
@@ -8,3 +8,7 @@ console.log(heading1);
 let para = document.getElementsByTagName("p");
 console.dir(para);
 console.log(para);
+
+let firstElement = document.querySelector("p");
+console.dir(firstElement);
+
